8987caf369f4f95e67e30f0e78277476d7bd85c7
Dalai Felinto <dfelinto@gmail.com>
2015-06-19 16:36:37 -0300

    Print raw data

--- blends/model.processor.py
+++ blends/model.processor.py
@@ -126,7 +126,7 @@ if blendervr.is_virtual_environment():
                 x, y, z = info['matrix'].translation
 
                 # leaving here for debugging, I need to refresh myself on what is the swizzle needed in BlenderVR
-                self.logger.info('x: {0:.2f}, y: {1:.2f}, z: {2:.2f}'.format(-x, z, -y))
+                self.logger.info('Raw Data >> x: {0:.2f}, y: {1:.2f}, z: {2:.2f}'.format(x, y, z))
 
                 position = Matrix.Translation((-x, z, -y)).translation
 
