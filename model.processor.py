79f2436f835cc9353db26208d989916b1a5d53a0
David Poirier-Quinot <davipoir@gmail.com>
2015-06-29 14:01:44 +0200

    added _keyboard_calibration method (manual calibration in bge) to .processor.py

--- model.processor.py
+++ model.processor.py
@@ -19,6 +19,7 @@ if blendervr.is_virtual_environment():
             self._all_loaded = False
             self._matrix = Matrix.Identity(4)
 
+            self._hacked = False
 
         def _checkScenes(self):
             """
@@ -62,10 +63,89 @@ if blendervr.is_virtual_environment():
             """
             run every frame
             """
+
+            self._keyboard_calibration()
+
             if not self._checkScenes():
                 return
 
             self._keyboard()
+
+        def _keyboard_calibration(self):
+            """handle keyboard events"""
+
+            """
+            Keyboard controls the camera representing the projector in the scene
+            (logic.scenes['Scene.Projection'].objects['Camera.Projector'])
+            with UJ/IK/OL (y,x,z) + Shift to switch from translation to rotation
+            and P/M to incr./decr. camera.lens
+            """
+            _events = logic.keyboard.events
+
+            x = y = z = 0
+            SPEED = 0.005
+            zoom = 0
+            SPEED_ZOOM = 0.05
+            log = False
+
+            if _events[events.UKEY]:
+                y -= SPEED
+
+            elif _events[events.JKEY]:
+                y += SPEED
+
+            if _events[events.IKEY]:
+                x += SPEED
+
+            elif _events[events.KKEY]:
+                x -= SPEED
+
+            if _events[events.OKEY]:
+                z += SPEED
+
+            elif _events[events.LKEY]:
+                z -= SPEED
+
+            if _events[events.PKEY]:
+                zoom += SPEED_ZOOM
+
+
+            elif _events[events.MKEY]:
+                zoom -= SPEED_ZOOM
+
+            if _events[events.SPACEKEY]:
+                log = True
+
+            if x or y or z or zoom or log:
+                try:
+                    if hasattr(logic, 'scenes'):
+                        scene = logic.getCurrentScene()
+                        camera_projector = scene.objects['Camera.Projector']
+
+                        if _events[events.LEFTSHIFTKEY]: # camera rotation
+                            gain = 1.0
+                            camera_projector.applyRotation([gain*x, gain*y, gain*z], 0)
+
+                        else: # camera translation / lens
+                            gain = 1.0
+                            camera_projector.applyMovement([gain*x, gain*y, gain*z], 0)
+                            camera_projector.lens += zoom
+
+                        if log:
+                            from math import degrees as d
+
+                            pos = camera_projector.position
+                            ori = camera_projector.localOrientation.to_euler()
+
+                            self.logger.info('\n Current Camera.Projector parameters:')
+                            self.logger.info('Pos >> x: {0:.2f}, y: {1:.2f}, z: {2:.2f}'.format(pos.x, pos.y, pos.z))
+                            self.logger.info('Ori >> x: {0:.2f}, y: {1:.2f}, z: {2:.2f}'.format(d(ori.x), d(ori.y), d(ori.z)))
+                            self.logger.info('FoV >> {0:.2f} \n'.format(camera_projector.lens))
+
+                except Exception as E:
+                    self.logger.log_traceback(E)
+
+
 
         def _keyboard(self):
             """handle keyboard events"""
@@ -125,13 +205,18 @@ if blendervr.is_virtual_environment():
 
                 x, y, z = info['matrix'].translation
 
-                # leaving here for debugging, I need to refresh myself on what is the swizzle needed in BlenderVR
-                self.logger.info('Raw Data >> x: {0:.2f}, y: {1:.2f}, z: {2:.2f}'.format(x, y, z))
+                if abs(x)>=0.01 or abs(y)>=0.01 or abs(z)>=0.01:
+                # to remove: mockup as VRPN sever kept sending me (0,0,0) packets in between real (x,y,z) (DPQ)
 
-                position = Matrix.Translation((-x, z, -y)).translation
+                    # leaving here for debugging, I need to refresh myself on what is the swizzle needed in BlenderVR
+                    self.logger.info('Raw Data >> x: {0:.2f}, y: {1:.2f}, z: {2:.2f}'.format(x, y, z))
 
-                self._headtrack_vr_head.worldPosition = position + self._headtrack_vr_origin.worldPosition
-                self._headtrack_projection_head.worldPosition = position + self._headtrack_projection_origin.worldPosition
+                    scale_real_2_vr = 1.0
+                    position = scale_real_2_vr * Matrix.Translation((z, x, y)).translation
+
+                    self._headtrack_vr_head.worldPosition = position + self._headtrack_vr_origin.worldPosition
+                    self.logger.info(self._headtrack_vr_head.worldPosition)
+                    self._headtrack_projection_head.worldPosition = position + self._headtrack_projection_origin.worldPosition
 
             except Exception as E:
                 self.logger.log_traceback(E)
